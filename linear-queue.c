82ea9e5f33b38eee7d70bf549d40b8cd0e56fdc0
ketarubot <robin09v@naver.com>
2025-08-04 12:27:20 +0900

    리팩토링

--- linear-queue.c
+++ linear-queue.c
@@ -1,3 +1,44 @@
-//
-// Created by taehoon on 25. 8. 3.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void enqueue(int x);
+void dequeue();
+
+int *queue;
+int front = -1, rear = -1;
+int length;
+int main(void) {
+    scanf("%d", &length);
+    queue = malloc(sizeof(int) * length);
+
+    // enqueue {x}, dequeue 이외의 입력은 모두 종료
+    while (1) {
+        char cmd[11];
+        scanf("%s", cmd);
+        if (strcmp(cmd, "enqueue") == 0) {
+            int num;
+            scanf("%d", &num);
+            enqueue(num);
+        } else if (strcmp(cmd, "dequeue") == 0) {
+            dequeue();
+        } else break;
+    }
+    free(queue);
+}
+
+void enqueue(int x) {
+    if (rear >= length-1) {
+        printf("Queue is full\n");
+    } else {
+        queue[++rear] = x;
+    }
+}
+
+void dequeue() {
+    if (front+1 > rear) {
+        printf("Queue is empty\n");
+    } else {
+        printf("%d\n", queue[++front]);
+    }
+}
--- stack.c
+++ stack.c
@@ -1,3 +1,44 @@
-//
-// Created by taehoon on 25. 8. 3.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void push(int x);
+void pop();
+
+int *stack;
+int rear = -1;
+int length;
+int main(void) {
+    scanf("%d", &length);
+    stack = malloc(sizeof(int) * length);
+
+    // push {x}, pop 이외의 입력은 모두 종료
+    while (1) {
+        char cmd[11];
+        scanf("%10s", cmd);
+        if (strcmp(cmd, "push") == 0) {
+            int src;
+            scanf("%d", &src);
+            push(src);
+        } else if (strcmp(cmd, "pop") == 0) {
+            pop();
+        } else break;
+    }
+    free(stack);
+}
+
+void push(int x) {
+    if (rear >= length-1) {
+        printf("Stack is full\n");
+    } else {
+        stack[++rear] = x;
+    }
+}
+
+void pop() {
+    if (rear <= -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("%d\n", stack[rear--]);
+    }
+}
